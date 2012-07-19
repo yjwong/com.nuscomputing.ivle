0fd1e198e715f9aeb63b450c42c97534fb42d036
Yong Jie Wong <yjwong92@gmail.com>
2012-07-20 06:29:08 +0800

    Added notifications support.

--- src/com/nuscomputing/ivle/IVLESyncService.java
+++ src/com/nuscomputing/ivle/IVLESyncService.java
@@ -1,13 +1,24 @@
 package com.nuscomputing.ivle;
 
+import com.nuscomputing.ivle.providers.AnnouncementsContract;
+
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.ContentProviderClient;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
 
 /**
  * The primary sync adapter for all announcements.
@@ -131,6 +142,7 @@ public class IVLESyncService extends Service {
 		Intent intent = new Intent(IVLESyncService.ACTION_SYNC_SUCCESS);
 		intent.putExtra("com.nuscomputing.ivle.Account", account);
 		context.sendBroadcast(intent);
+		IVLESyncService.onSyncSuccess(context, account);
 	}
 	
 	/**
@@ -157,6 +169,124 @@ public class IVLESyncService extends Service {
 		Intent intent = new Intent(IVLESyncService.ACTION_SYNC_FAILED);
 		intent.putExtra("com.nuscomputing.ivle.Account", account);
 		context.sendBroadcast(intent);
+	}
+	
+	/**
+	 * Method: onSyncSuccess
+	 * <p>
+	 * Called when the sync was successful. Currently used to display
+	 * notifications.
+	 */
+	@SuppressWarnings("deprecation")
+	@TargetApi(16)
+	private static void onSyncSuccess(Context context, Account account) {
+		// Check if we enable notifications.
+		SharedPreferences prefs = context.getSharedPreferences("account_" + account.name, Context.MODE_PRIVATE);
+		boolean notifsEnabled = prefs.getBoolean("notifications", true);
+		boolean notifsAnnouncementsEnabled = prefs.getBoolean("notifications_announcements", true);
+		
+		if (notifsEnabled) {
+			if (notifsAnnouncementsEnabled) {
+				// Get unread announcements.
+				ContentResolver resolver = context.getContentResolver();
+				ContentProviderClient provider = resolver.acquireContentProviderClient(AnnouncementsContract.CONTENT_URI);
+				try {
+					Cursor cursor = provider.query(
+							AnnouncementsContract.CONTENT_URI,
+							new String[] { AnnouncementsContract.ID, AnnouncementsContract.TITLE },
+							AnnouncementsContract.IS_READ.concat(" = ?").concat(" AND ")
+								.concat(DatabaseHelper.ANNOUNCEMENTS_TABLE_NAME).concat(".").concat(AnnouncementsContract.ACCOUNT).concat(" = ?"),
+							new String[] { "0", account.name },
+							null
+					);
+					
+					// Get the notification manager.
+					NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+					int announcementCount = cursor.getCount();
+					
+					// Determine how we show the notification based on the
+					// number of announcements.
+					if (announcementCount > 0) {
+						Notification notif = null;
+						cursor.moveToFirst();
+						
+						if (announcementCount == 1) {
+							// Obtain the details for the only announcement.
+							String notifTitle = cursor.getString(cursor.getColumnIndex(AnnouncementsContract.TITLE));
+							long announcementId = cursor.getLong(cursor.getColumnIndex(AnnouncementsContract.ID));
+							
+							// Create a pending intent.
+							Intent intent = new Intent(context, ViewAnnouncementActivity.class);
+							intent.putExtra("announcementId", announcementId);
+							PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+							
+							// Create notification based on platform version.
+							if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+								Notification.Builder builder = new Notification.Builder(context)
+									.setContentTitle(notifTitle)
+									.setContentText(account.name)
+									.setContentIntent(pendingIntent)
+									.setSmallIcon(R.drawable.ic_launcher);
+								
+								notif = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) ? builder.build() : builder.getNotification();
+							} else {
+								notif = new Notification(R.drawable.ic_launcher, notifTitle, System.currentTimeMillis());
+								notif.setLatestEventInfo(context, notifTitle, account.name, pendingIntent);
+							}
+	
+						} else {
+							String notifTitle = Integer.toString(announcementCount).concat(" new announcements");
+							
+							// Create a pending intent.
+							Intent intent = new Intent(context, MainActivity.class);
+							PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+							
+							// Create notification based on platform version.
+							if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+								Notification.Builder builder = new Notification.Builder(context)
+									.setContentTitle(notifTitle)
+									.setContentText(account.name)
+									.setContentIntent(pendingIntent)
+									.setSmallIcon(R.drawable.ic_launcher);
+								
+								// Use the inbox style on Jellybean.
+								if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+									Notification.InboxStyle builderInboxStyle = new Notification.InboxStyle(builder);
+									
+									// We also want to limit the number of lines to 5.
+									int a = 0;
+									while (!cursor.isAfterLast() && a < 5) {
+										builderInboxStyle.addLine(cursor.getString(cursor.getColumnIndex(AnnouncementsContract.TITLE)));
+										cursor.moveToNext();
+										a++;
+									}
+									
+									// Tap to show the rest.
+									if (announcementCount > 5) {
+										builderInboxStyle.setSummaryText("+" + (announcementCount - 5) + " more");
+									}
+									
+									notif = builderInboxStyle.build();
+									
+								} else {
+									notif = builder.getNotification();
+								}
+								
+							} else {
+								notif = new Notification(R.drawable.ic_launcher, notifTitle, System.currentTimeMillis());
+								notif.setLatestEventInfo(context, notifTitle, account.name, pendingIntent);
+							}
+						}
+						
+						// Send the notification.
+						manager.notify(0, notif);
+					}
+					
+				} catch (RemoteException e) {
+					Log.e(TAG, "Error trying to check for unread announcements");
+				}
+			}
+		}
 	}
 	
 	// }}}
